ee1d2ae76fdd6846b38ca0aad02e1a3579007fd7
Lê Anh Phi - TCIS <phila@saigonnewport.com.vn>
2025-02-25 09:10:24 +0700

    update readme

--- Models/TelegramOptions.cs
+++ Models/TelegramOptions.cs
@@ -5,6 +5,7 @@ public class TelegramOptions
     public string ErrorChannelId { get; set; } = string.Empty;
     public Dictionary<string, string> Formats { get; set; } = [];
     public int MaxMessageLength { get; set; } = 4096;
-    public int MaxRetryAttempts { get; set; } = 3;
     public string BotToken { get; set; } = string.Empty;
+
+    public int MaxRetryAttempts { get; set; } = 3;
 }
